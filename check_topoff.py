4041836b15900a21e1bd2dfe3c5f88d13506e9a5
jsomville@hotmail.com <jsomville@hotmail.com>
2026-03-22 19:26:40 +0100

    whole process works with transaction date filter

--- check_topoff.py
+++ check_topoff.py
@@ -1,33 +1,18 @@
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 import os
 import traceback
 
-from getBankInfo import get_access_token, get_account_id, get_account_transactions, get_bank_account_details, process_transactions
+from getBankInfo import call_account_transactions, call_bank_account_details, call_get_access_token, get_account_id, get_bank_account_summary
+from transaction_filter import filter_transactions
 from setPayment import process_payment
 from logger import log_this, config_logging
 
 debug_this = False
 
-last_processed_transaction_id_file = "last_processed_transaction_id.txt"
-
-def filter_transactions(transactions, last_processed_transaction_id):
-    if last_processed_transaction_id is None:
-        return transactions
-    else:
-        transaction_to_process = []
-        for transaction in transactions:
-            #Is this by order DESC ?
-            if transaction['id'] == last_processed_transaction_id:
-                break
-            else:
-                transaction_to_process.append(transaction)
-    
-    if debug_this:
-        print(f"Filtered Transactions from {len(transactions)} to {len(transaction_to_process)}")         
-    
-    return transaction_to_process
+last_day_to_check= int(os.getenv("LAST_DAYS_TO_CHECK", 1))
+FILTER_DATE = datetime.now() - timedelta(days=last_day_to_check)
 
 def save_unprocessed_transactions(unprocessed_transactions):
 
@@ -35,7 +20,7 @@ def save_unprocessed_transactions(unprocessed_transactions):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    date_formatted = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    date_formatted = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     user_file_path = os.path.join(path, f"{date_formatted}_unprocessed_transactions.json")
     with open(user_file_path, "w") as f:
         json.dump(unprocessed_transactions, f, indent=2, ensure_ascii=False)
@@ -47,37 +32,35 @@ def main():
         
         log_this("info", "Bank watch started")
         
-        access_token = get_access_token()
+        #Get the access token for the bank API
+        access_token = call_get_access_token()
         if (access_token):
         
             #get the bank information
-            bank_detail = get_bank_account_details(access_token)
+            raw_account_list = call_bank_account_details(access_token)
+            account_list = get_bank_account_summary(raw_account_list)
         
             #from the IBAN, get the bank accountid
-            bank_id = os.getenv("BANK_ACCOUNT_IBAN")
-            account_id = get_account_id(bank_detail, bank_id)
+            iban_ref = os.getenv("BANK_ACCOUNT_IBAN")
+            account_id = get_account_id(account_list, iban_ref)
+            
+            #Check for Bank Account ID
+            if (account_id is None):
+                log_this("error", f"Account with IBAN {iban_ref} not found")
+                raise ValueError(f"Account with IBAN {iban_ref} not found")
         
             #get the transactions for the account
-            transactions = get_account_transactions(access_token, account_id)
-        
-            #get last processed Transaction ID from file if exists
-            if last_processed_transaction_id_file in os.listdir():
-                with open(last_processed_transaction_id_file, "r") as f:
-                    last_processed_transaction_id = f.read()
-            else:
-                last_processed_transaction_id = None
-            log_this("info", f"Last processed transaction id: {last_processed_transaction_id}")
+            raw_transactions_list = call_account_transactions(access_token, account_id)
             
-            #process the transactionns
-            transactions = process_transactions(transactions)
-            transactions_to_process = filter_transactions(transactions, last_processed_transaction_id)
-            if len(transactions_to_process) > 0:
+            #process the transactions
+            transactions_list = filter_transactions(raw_transactions_list, FILTER_DATE)
+            if len(transactions_list) > 0:
                 
-                log_this("info", f"There are {len(transactions_to_process)} transactions to process")
+                log_this("info", f"There are {len(transactions_list)} transactions to process")
                 
                 unprocessed_transactions = []
                 
-                for transaction in transactions_to_process:
+                for transaction in transactions_list:
                     if debug_this:
                         print(f"Processing transaction {json.dumps(transaction, indent=2)}")
                     
@@ -107,11 +90,6 @@ def main():
                     save_unprocessed_transactions(unprocessed_transactions)
                 else:
                     log_this("info", "All transactions processed successfully")
-                    
-                #Save last processed transaction id to file
-                if last_processed_transaction_id:
-                    with open(last_processed_transaction_id_file, "w") as f:
-                        f.write(last_processed_transaction_id)
             else:
                 log_this("info", "No new transactions to process")
         
--- transaction_filter.py
+++ transaction_filter.py
@@ -3,21 +3,21 @@ from logger import log_this, config_logging
 import json
 import traceback
 import re
-from datetime import datetime
+from datetime import date, datetime, timedelta, timezone
 
 load_dotenv()
 
 debug_this = False
 debug_transactions = False
 
-def get_filtered_transactions(transactions):
+def filter_transactions(transactions, date_from):
     """Process transactions and extract relevant information"""
     try:
         processed = []
         counter = 0
         for transaction in transactions.get("data", []):
             
-            processed_transaction = filter_transaction(transaction)
+            processed_transaction = filter_transaction(transaction, date_from)
             if processed_transaction["status"] == "success":
                 processed.append(processed_transaction)
                 
@@ -35,7 +35,7 @@ def get_filtered_transactions(transactions):
         log_this("error", f"Error processing transactions: {message}")
         raise
 
-def filter_transaction(transaction):
+def filter_transaction(transaction, date_from):
     """Process a single transaction and extract relevant information"""
     try:
         attributes = transaction.get("attributes", {})
@@ -59,14 +59,19 @@ def filter_transaction(transaction):
             transaction_result["reason"] = "Missing date"
             return transaction_result
         try:
-            date_json = json.dumps(date_str)
             date = datetime.fromisoformat(date_str.replace("Z", "+00:00"))
+            transaction_result["date"] = date.isoformat()
         except ValueError:
             log_this("error", f"Invalid date format: {date_str}")
             transaction_result["status"] = "failed"
             transaction_result["reason"] = "Invalid date format"
             return transaction_result
-        transaction_result["date"] = date.isoformat()
+        
+        transaction_date = datetime.fromisoformat(transaction_result["date"])
+        if transaction_date and transaction_date.date() < date_from.date():
+            transaction_result["status"] = "failed"
+            transaction_result["reason"] = "Transaction date is before the specified date_from"
+            return transaction_result
         
         #Check Amount
         amount = attributes.get("amount")
@@ -114,24 +119,24 @@ def filter_transaction(transaction):
         log_this("error", f"Error processing transaction: {message}")
         raise
     
-def test_filter_transaction(transaction_data, testNumber, expected_result):
+def test_filter_transaction(transaction_data, testNumber, test_name, filter_date, expected_result):
     """Test the filter_transaction function with given data and expected result"""
     try:
-        result = filter_transaction(transaction_data)
+        result = filter_transaction(transaction_data, filter_date)
         if result["status"] == "success" and expected_result == True:
-            print(f"Test {testNumber} passed")
+            print(f"Test {testNumber} passed - {test_name} ")
             return True
         
         if result["status"] == "failed" and expected_result == False:
-            print(f"Test {testNumber} passed")
+            print(f"Test {testNumber} passed - {test_name} ")
             return True
         
         if result["status"] == "failed" and expected_result == True:
-            print(f"Test {testNumber} failed: Expected success, got {result}")
+            print(f"Test {testNumber} - {test_name} : failed: Expected success, got {result}")
             return False
         
         if result["status"] == "success" and expected_result == False:
-            print(f"Test {testNumber} failed: Expected failure, got {result}")
+            print(f"Test {testNumber} - {test_name} : failed: Expected failure, got {result}")
             return False
 
     except Exception as error:
@@ -142,7 +147,12 @@ def execute_test_cases():
     test_ok = True
     test_count = 0
     
+    filterdate = datetime.now() - timedelta(days=1)
+    
+    yesterday = datetime.now(timezone.utc) - timedelta(days=1)
+    
     #Test - OK
+    test_name = "OK with valid data"
     test_count+=1
     test_data = {
         "id": "12345",
@@ -150,15 +160,16 @@ def execute_test_cases():
             "amount": 100.0,
             "currency": "EUR",
             "remittanceInformation": "123-4567-89012",
-            "executionDate": "2024-06-01T23:00:00.000Z"
+            "executionDate": yesterday.isoformat(timespec="milliseconds").replace("+00:00", "Z")
         }
     }
-    r = test_filter_transaction(test_data, test_count, True)
+    r = test_filter_transaction(test_data, test_count, test_name,filterdate, True)
     if not r:
         test_ok = False
 
     
     # Test - OK with full digit remittance information
+    test_name = "OK with full digit remittance information"
     test_count+=1
     test_data = { 
         "id": "12345",
@@ -166,145 +177,203 @@ def execute_test_cases():
             "amount": 100.0,
             "currency": "EUR",
             "remittanceInformation": "123456789012",
-            "executionDate": "2024-06-01T23:00:00.000Z"
+            "executionDate": yesterday.isoformat(timespec="milliseconds").replace("+00:00", "Z")
         }
     }
-    r = test_filter_transaction(test_data, test_count, True)
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, True)
     if not r:
         test_ok = False
         
     # Test - OK with full digit with slash
     test_count+=1
+    test_name = "OK with full digit with slash"
     test_data = { 
         "id": "12345",
         "attributes": {
             "amount": 100.0,
             "currency": "EUR",
             "remittanceInformation": "123/4567/89012",
-            "executionDate": "2024-06-01T23:00:00.000Z"
+            "executionDate": yesterday.isoformat(timespec="milliseconds").replace("+00:00", "Z")
         }
     }
-    r = test_filter_transaction(test_data, test_count, True)
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, True)
     if not r:
         test_ok = False
         
     # Test - OK with full digit with underscore
     test_count+=1
+    test_name = "OK with full digit with underscore"
     test_data = { 
         "id": "12345",
         "attributes": {
             "amount": 100.0,
             "currency": "EUR",
             "remittanceInformation": "123_4567_89012",
-            "executionDate": "2024-06-01T23:00:00.000Z"
+            "executionDate": yesterday.isoformat(timespec="milliseconds").replace("+00:00", "Z")
         }
     }
-    r = test_filter_transaction(test_data, test_count, True)
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, True)
     if not r:
         test_ok = False
     
     # Test - OK with full digit with space
     test_count+=1
+    test_name = "OK with full digit with space"
     test_data = { 
         "id": "12345",
         "attributes": {
             "amount": 100.0,
             "currency": "EUR",
             "remittanceInformation": "123 4567 89012",
-            "executionDate": "2024-06-01T23:00:00.000Z"
+            "executionDate": yesterday.isoformat(timespec="milliseconds").replace("+00:00", "Z")
         }
     }
-    r = test_filter_transaction(test_data, test_count, True)
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, True)
     if not r:
         test_ok = False
     
     #Test - Not ok missing remitance
     test_count+=1
+    test_name = "Not ok missing remitance"
     test_data = {
         "id": "12345",
         "attributes": {
             "amount": 100.0,
             "currency": "EUR",
             "remittanceInformation": "",
-            "executionDate": "2024-06-01T23:00:00.000Z"
+            "executionDate": yesterday.isoformat(timespec="milliseconds").replace("+00:00", "Z")
         }
     }
-    r = test_filter_transaction(test_data, test_count, False)
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, False)
     if not r:
         test_ok = False
     
-    #Test - Not ok remotance format
+    #Test - Not ok remittance format
     test_count+=1
+    test_name = "Not ok remittance format"
     test_data = {
         "id": "12345",
         "attributes": {
             "amount": 100.0,
             "currency": "EUR",
             "remittanceInformation": "12-123-154556",
-            "executionDate": "2024-06-01T23:00:00.000Z"
+            "executionDate": yesterday.isoformat(timespec="milliseconds").replace("+00:00", "Z")
         }
     }
-    r = test_filter_transaction(test_data, test_count, False)
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, False)
     if not r:
         test_ok = False
         
-    #Test - Not ok
+    #Test - Not ok remittance format with letters
     test_count+=1
+    test_name = "Not ok remittance format with letters"
     test_data = {
         "id": "12345",
         "attributes": {
             "amount": 100.0,
             "currency": "EUR",
             "remittanceInformation": "A12-123-154556",
-            "executionDate": "2024-06-01"
+            "executionDate": yesterday.isoformat(timespec="milliseconds").replace("+00:00", "Z")
         }
     }
-    r = test_filter_transaction(test_data, test_count, False)
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, False)
     if not r:
         test_ok = False
     
     #Test - Not ok negative amount
     test_count+=1
+    test_name = "Not ok negative amount"
     test_data = {
         "id": "12345",
         "attributes": {
             "amount": -100.0,
             "currency": "EUR",
             "remittanceInformation": "112-123-154556",
-            "executionDate": "2024-06-01T23:00:00.000Z"
+            "executionDate": yesterday.isoformat(timespec="milliseconds").replace("+00:00", "Z")
         }
     }
-    r = test_filter_transaction(test_data, test_count, False)
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, False)
     if not r:
         test_ok = False
 
-    #Test - Not ok date only
+    #Test - date only
     test_count+=1
+    test_name = "Date only"
     test_data = {
         "id": "12345",
         "attributes": {
-            "amount": 0,
+            "amount": 10,
             "currency": "EUR",
             "remittanceInformation": "112-123-154556",
-            "executionDate": "2024-06-01"
+            "executionDate": yesterday.date().isoformat()
         }
     }
-    r = test_filter_transaction(test_data, test_count, False)
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, True)
+    if not r:
+        test_ok = False
+        
+    #Test - Not ok date + time only
+    test_count+=1
+    test_name = "Date + time"
+    test_data = {
+        "id": "12345",
+        "attributes": {
+            "amount": 10,
+            "currency": "EUR",
+            "remittanceInformation": "112-123-154556",
+            "executionDate": yesterday.strftime("%Y-%m-%dT%H:%M:%S")
+        }
+    }
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, True)
+    if not r:
+        test_ok = False
+    
+     #Test - Not ok date + time only
+    test_count+=1
+    test_name = "Date time with space"
+    test_data = {
+        "id": "12345",
+        "attributes": {
+            "amount": 10,
+            "currency": "EUR",
+            "remittanceInformation": "112-123-154556",
+            "executionDate": yesterday.strftime("%Y-%m-%d %H:%M:%S")
+        }
+    }
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, True)
     if not r:
         test_ok = False
     
     #Test - Not ok date + time no TZ
     test_count+=1
+    test_name = "Datetime no TZ"
     test_data = {
         "id": "12345",
         "attributes": {
-            "amount": 0,
+            "amount": 10,
             "currency": "EUR",
             "remittanceInformation": "112-123-154556",
-            "executionDate": "2024-06-01T23:00:00"
+            "executionDate": yesterday.isoformat(timespec="milliseconds")
         }
     }
-    r = test_filter_transaction(test_data, test_count, False)
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, True)
+    if not r:
+        test_ok = False
+    
+    #Test - Not ok date + time no TZ
+    test_count+=1
+    test_name = "Too late"
+    too_late = datetime.now(timezone.utc) - timedelta(days=2)
+    test_data = {
+        "id": "12345",
+        "attributes": {
+            "amount": 10,
+            "currency": "EUR",
+            "remittanceInformation": "112-123-154556",
+            "executionDate": too_late.isoformat(timespec="milliseconds").replace("+00:00", "Z")
+        }
+    }
+    r = test_filter_transaction(test_data, test_count, test_name, filterdate, False)
     if not r:
         test_ok = False
 
