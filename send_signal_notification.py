14e04387cd3e6ab80ce6a1d547e4d7142a15bc3f
jsomville@hotmail.com <jsomville@hotmail.com>
2026-03-22 16:58:47 +0100

    add signal notification

---
+++ send_signal_notification.py
@@ -0,0 +1,64 @@
+import json
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+API_URL = os.getenv("SIGNAL_URL")
+SENDER_NUMBER = os.getenv("SIGNAL_SOURCE_NUMBER")
+
+def send_signal_message(sender_number, recipients, api_url, message, timeout=10):
+    """Send a message using the Signal REST API."""
+
+    url = f"{API_URL.rstrip('/')}/v2/send"
+    headers = {"Content-Type": "application/json"}
+    payload = {
+        "message": message,
+        "number": SENDER_NUMBER,
+        "recipients": recipients,
+    }
+
+    response = requests.post(url, headers=headers, json=payload, timeout=timeout)
+    response.raise_for_status()
+
+    if response.text:
+        return response.json()
+    return {"status": "ok", "status_code": response.status_code}
+
+def list_group(timeout=10):
+    """List groups using the Signal REST API."""
+    """Use this to identify the group ID for the destination group."""
+
+    url = f"{API_URL.rstrip('/')}/v1/groups/{SENDER_NUMBER}"
+    headers = {"Content-Type": "application/json"}
+    
+    response = requests.get(url, headers=headers, timeout=timeout)
+    response.raise_for_status()
+
+    if response.text:
+        data = response.json()
+        print(json.dumps(data, indent=2, ensure_ascii=False))
+    return {"status": "ok", "status_code": response.status_code}
+
+
+if __name__ == "__main__":
+   
+    #*********************************
+    destination_group = os.getenv("SIGNAL_DESTINATION_GROUP")
+    recipients = []
+    recipients.append(destination_group)
+    
+    message = "Test message via signal-cli!"
+    #*********************************
+    
+    print(f"Sending message from {SENDER_NUMBER} to {recipients} via Signal API...")
+    
+    result = send_signal_message(
+        recipients=recipients,
+        message=message,
+    )
+    print(json.dumps(result, indent=2, ensure_ascii=False))
+    
+    ##list_group()
+    
