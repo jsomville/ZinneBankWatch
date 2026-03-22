2dd8d03832c153d9629bc6e78508fef713c3ebae
jsomville@hotmail.com <jsomville@hotmail.com>
2026-03-22 16:56:18 +0100

    add log folder

--- logger.py
+++ logger.py
@@ -1,11 +1,20 @@
+from datetime import datetime
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
 def config_logging():
     """Configure logging settings"""
+    
+    #Create Log folder
+    if not os.path.exists("log"):
+        os.makedirs("log")
+    
+    today = datetime.now().strftime("%Y-%m-%d")
+    
     logging.basicConfig(
-        filename="bank_watch.log",
+        filename=f"log/{today}_bank_watch.log",
         level=logging.INFO,
         format="%(asctime)s %(levelname)s %(name)s: %(message)s",
         encoding="utf-8"             
